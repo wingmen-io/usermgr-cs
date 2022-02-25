3cf59736390b2809844f91ea3a4b8d13d042f5ec
Paul <paul.vanwingerden@hm.com>
2022-02-25 22:49:42 +0100

    added a tab of polish
    - added nuget config that blocks private feeds
    - moved code to src folder
    - changed .gitignore so it catches built output (copied relevant part of
        the .gitignore that is created after running dotnet new gitignore)
    
    - added editorconfig and ran codecleanup. editorconfig alo contains the definitions that in the
      past were in rulesets. This controls Analyzers and Fixers, "linters on steroids".
    
    - generate assemblyinfo file to add properties to the binaries.
    
      check with this command: pwsh -command "get-item *.exe | select-object name,{$_.versioninfo.fileversion
      or to see all properties remove the part after |
    
      Refs:
      https://docs.microsoft.com/en-us/dotnet/core/project-sdk/msbuild-props#generatedassemblyinfofile
    
    - added Directory.Build.Props to control the build definitions outside of the csproj file.
      NB. This is handy in case you have a lot of different projects that you want to centrally configure.
          It supports hierarchy so on a project basis you can override stuff.
    
    - changed Newtonsoft JSON handling to System.Text.Json

--- src/Config.cs
+++ src/Config.cs
@@ -4,17 +4,19 @@ namespace Aserto.UserManager
 {
     public record Config
     {
-        public Aserto.Config Aserto {get; init;}
+        public Aserto.Config Aserto { get; init; }
 
-        public Okta.Config Okta {get; init;}
+        public Okta.Config Okta { get; init; }
 
-        public Config() {
+        public Config()
+        {
             var cfg = DotEnv.Fluent()
                 .WithExceptions()
                 .WithEnvFiles()
                 .Read();
 
-            Aserto = new Aserto.Config{
+            Aserto = new Aserto.Config
+            {
                 TenantID = cfg["ASERTO_TENANT_ID"],
                 AuthorizerAddr = cfg["ASERTO_AUTHORIZER_ADDR"],
                 AuthorizerAPIKey = cfg["ASERTO_AUTHORIZER_API_KEY"]
---
+++ src/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+
+namespace Aserto.UserManager
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var userOption = new Option<string>(
+                "--user-id",
+                getDefaultValue: () => string.Empty,
+                description: "user identifier (email or provider ID)");
+
+            var rootCommand = new RootCommand
+            {
+                userOption
+            };
+
+            rootCommand.Description = "User Manager";
+
+            rootCommand.SetHandler(async (string userID) => {
+                var config = new Config();
+                var producer = new Okta.Producer(config.Okta);
+                var subscriber = new Aserto.Subscriber(config.Aserto);
+
+                var asertoUsers = Okta.Producer.Transform(
+                    (string.IsNullOrEmpty(userID) ? producer.Users() : producer.UserByID(userID)));
+
+                await foreach (var asertoUser in asertoUsers)
+                {
+                    asertoUser.Dump();
+                }
+
+                var result = await subscriber.LoadUsers(asertoUsers);
+
+                Console.WriteLine("received {0}", result.Received);
+                Console.WriteLine("created  {0}", result.Created);
+                Console.WriteLine("updated  {0}", result.Updated);
+                Console.WriteLine("deleted  {0}", result.Deleted);
+                Console.WriteLine("errors   {0}", result.Errors);
+
+            }, userOption);
+
+            await rootCommand.InvokeAsync(args);
+        }
+    }
+}
